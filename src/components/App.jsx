741213b2db8fa6e6055ea78f6a52d9fa970c833c
VitiukP <pasha2002vityuk@gmail.com>
2023-09-05 14:06:39 +0300

    HW-Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import { Profile } from "./Profile/Profile";
+import user from "../data/user.json";
+import { Statistics } from './Statistics/Statistics.jsx';
+import data from '../data/data.json';
+import { FriendList } from "./FriendList/FriendList";
+import friends from '../data/friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+
+const rootStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '40px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 40,
+  color: '#010101',
+};
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div style={rootStyles}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+</div>
+    
+
+
   );
 };
